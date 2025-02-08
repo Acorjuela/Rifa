0fda7aff10b893c09af2a1820337b16579d80d63
Acorjuela <105765061+Acorjuela@users.noreply.github.com>
2025-02-08 09:40:33 -0500

    Update java.js

--- java.js
+++ java.js
@@ -5,7 +5,7 @@ const modal = document.getElementById('modal');
 const modalForm = document.getElementById('modalForm');
 
 // URL de Google Sheets
-const SHEET_URL = "https://api.sheetbest.com/sheets/89daa799-cf8e-47f7-bdc2-b9e298939474";
+const SHEET_URL = "https://api.sheetbest.com/sheets/7cb73c8e-3e86-4081-887d-a173ec48272b";
 
 let numerosOcupados = [];
 
